62b7104684a298d7b79d7c7c86ff7d44ebf854d7
Franc[e]sco <lolisamurai@tfwno.gf>
2017-06-22 05:20:06 +0200

    add packaging script, switch to static build

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+git pull origin master
+
+echo -e "\nCompiling and Stripping"
+LDFLAGS="$LDFLAGS -static -s -w $(pkg-config --static --libs gtk+-2.0)"
+go build --ldflags "-linkmode external -extldflags '$LDFLAGS'"
+
+echo -e "\nPackaging"
+folder="sharenix-$(uname -m)"
+mkdir -p "$folder"
+mv ./sharenix $folder/sharenix
+cp ./sharenix.json $folder/sharenix.json
+
+rm "$folder".tar.xz
+tar -cvJf "$folder".tar.xz \
+    "$folder"/sharenix \
+    "$folder"/sharenix.json
+
+echo -e "\nResult:"
+tar tf "$folder".tar.xz
+
